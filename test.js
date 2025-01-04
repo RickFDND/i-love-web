6a88f5e79fb4e0ba158d675c49640bbcaaeb9f1c
RickFDND <rick.pijnacker@hva.nl>
2025-01-04 17:05:35 +0100

    2 paarden kunnen eruit en een rook linkt naar learning journal

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+//Blackpawn4 twee stappen vooruit//
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.blackpawn4');
     
@@ -6,8 +7,27 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+//Whitepawn4 twee stappen vooruit//
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.whitepawn4');
+    
+    button.addEventListener('click', function() {
+        button.classList.toggle('active');
+    });
+});
+
+//Whiteknight1 eruit//
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.whiteknight1');
+    
+    button.addEventListener('click', function() {
+        button.classList.toggle('active');
+    });
+});
+
+//Blackknight2 eruit//
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.blackknight2');
     
     button.addEventListener('click', function() {
         button.classList.toggle('active');
