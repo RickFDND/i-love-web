2cfc5ec87c82aed9a937b2fdcc9696e084802863
RickFDND <rick.pijnacker@hva.nl>
2025-04-04 15:15:27 +0200

    huts

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 // Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
 import express from 'express'
 
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from 'liquidjs';
+
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
 
@@ -9,8 +12,26 @@ const app = express()
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
 
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine('liquid', engine.express()); 
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set('views', './views')
+
 // Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({extended: true}))
+
+//home.liquid
+app.get('/', function(request,response){
+    response.render('home.liquid')
+})
+
+//voortgang.liquid
+app.get('/voortgang', function(request,response){
+    response.render('voortgang.liquid')
+})
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
